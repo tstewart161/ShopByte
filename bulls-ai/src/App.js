6e2b88aab95724dc0a9dd500686786de0d71cfd7
tstewart161 <1.61tstewart@gmail.com>
2020-04-18 23:23:30 -0400

    Quickstart

--- bulls-ai/src/App.js
+++ bulls-ai/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Wooto from './components/Wooto';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Wooto name="Wooto"/>
         <a
           className="App-link"
           href="https://reactjs.org"
