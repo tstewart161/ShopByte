c231dd5d1a06709f08a89383d0d510522af8a720
tstewart161 <1.61tstewart@gmail.com>
2020-06-02 00:48:42 -0400

    Added API skeleton

--- server.js
+++ server.js
@@ -13,7 +13,9 @@
 	-> Businesses:
 		- Adding new business.
 		- Adding a new product.
+		- Update product.
 		- Subscribing new business.
+		- Cancel subscription
 		- Selling a product.
 		- Allowing account withdrawal. (take withdrawal fee here or take it at sale?)(Allow this at all?)
 		- Current customer profiles.
@@ -21,3 +23,112 @@
 		- List current products in marketplace.
 		- Show current profile.
 */		
+
+const express = require('express');
+const stripe = require('stripe')('STRIPE_KEY');
+const http = require('http');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+let app = express();
+app.use(cors({origin: 'http://localhost:3000'}));
+app.use(bodyParser.json());
+let PORT = 3002;
+
+app.get('/', (req, res) => {
+	res.send('Server running');
+});
+
+
+/// Customers:
+///
+app.post('/create-user', async(req, res) => {
+
+});
+
+
+app.post('/create-profile', async(req, res) => {
+
+});
+
+
+app.post('/user-profile', async(req, res) => {
+
+});
+
+
+app.post('/update-profile', async(req, res) => {
+
+});
+
+
+app.post('/add-money', async(req, res) => {
+
+});
+
+app.post('/pay-users', async(req, res) => {
+
+});
+
+
+app.post('/purchase-product', (req, res) => {
+
+});
+
+
+app.post('/deliver-user-profile', async(req, res) => {
+
+});
+
+
+
+/// Businesses:
+///
+
+app.post('/create-business', async(req, res) => {
+
+});
+
+
+app.post('/create-product', async(req, res) => {
+
+});
+
+
+app.post('/update-product', async(req, res) => {
+
+});
+
+
+app.post('/create-business-subscription', async(req, res) => {
+
+});
+
+
+app.post('/cancel-subscription', async(req, res) => {
+
+});
+
+
+app.post('/sell-product', async(req, res) => {
+
+});
+
+
+// app.post('/account-withdrawal', async(req, res) => {
+
+// });
+
+
+app.post('/display-customer-profiles', async(req, res) => {
+
+});
+
+
+app.post('/current-products', async(req, res) => {
+
+});
+
+http.createServer(app).listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
